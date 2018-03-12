4160a9c30ebb2c96b9bf1c90286cd28be4eaabaa
Zach Johnson <zachtjohnson01@gmail.com>
2018-03-12 08:29:59 -0600

    Updated mysql connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+var connection;
+
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
